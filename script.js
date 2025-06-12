7ac055572bf561d9ed9a12ebd9722c2acce08897
jagan <72664113+jagannathan89@users.noreply.github.com>
2025-06-11 17:51:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -212,18 +212,4 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var currentYear = new Date().getFullYear();
     var copyrightText = document.querySelector(".footer .copyright .year").innerHTML
     document.querySelector(".footer .copyright .year").innerHTML = copyrightText.replace('year', currentYear);
-
-
-    <script>
-          let currentIndex = 0;
-          const slides = document.querySelectorAll('.slide');
-        
-          function showNextSlide() {
-            slides[currentIndex].classList.remove('active');
-            currentIndex = (currentIndex + 1) % slides.length;
-            slides[currentIndex].classList.add('active');
-          }
-        
-          setInterval(showNextSlide, 3000); // change slide every 3 seconds
-    </script>
 })
